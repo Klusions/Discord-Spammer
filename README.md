2048c92031f9681906823a2fadc8c94c8af4c735
Tim <56159391+TimsLab@users.noreply.github.com>
2019-11-20 21:17:11 +0100

    Add files via upload

---
+++ README.md
@@ -0,0 +1,27 @@
+<div align="center">
+  <br>
+  <img width="210" height="210" alt="Logo" src="https://i.imgur.com/0zjpP0u.png" />
+  <br>
+
+
+  <br>
+   <a href="#" target="_blank">
+<img  height="30" weight="30" src="https://image.spreadshirtmedia.net/image-server/v1/mp/designs/137963376,width=178,height=178/discord-logo.png"/>
+</a>  
+ <a href="#" target="_blank">
+<img  height="30" weight="30" src="https://cdn.iconscout.com/icon/free/png-256/youtube-88-227910.png"/>
+</a>
+ <a href="#" target="_blank">
+<img  height="30" weight="30" src="http://i.imgur.com/tXSoThF.png"/>
+</a>    
+  <br>
+ </div>
+
+![#DA70D6](https://placehold.it/15/48D1CC/000000?text=+) `Released at 20.11.2019`  
+
+<p align="center">
+  <img width="977" height="450" src="https://i.imgur.com/CRfNwUn.pngg">
+</p>
+<p align="center">
+  <img width="977" height="360" src="https://i.imgur.com/eb7U7lS.png">
+</p>
---
+++ index.php
@@ -0,0 +1,94 @@
+﻿<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN"> 
+<html>
+<head>
+<link rel="stylesheet" type="text/css" href="css/style.css"
+    <link href="https://fonts.googleapis.com/css?family=Montserrat|Nunito|Poppins&display=swap" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css?family=Amatic+SC&display=swap" rel="stylesheet">
+    </body>
+<title>Discord Spammer - TimsLab</title>
+</head>
+<br>
+<br>
+<br>
+<img class="center" src="css/logo.png" alt="TimsLab">
+
+<body onLoad="type_text()" ; bgColor=Black text=#00FFFF>
+<div style="width: 600px;height: 100px;" align="center">
+</div>
+</body>
+<div style="text-align: center;">
+<p id="response"></p>
+<p id="sending"><font color=white>Loading Stats...</p>
+<button class="button" onclick="load()">Load Tokens</button>
+<br>
+<input type="message1" id="invite" placeholder="Discord Invite Code"></br>
+<button class="button" onclick="join();">Join Server</button>
+<br>
+<input type="message" id="id" placeholder="Channel ID*">
+<input type="message" id="message" placeholder="Message*"></br>
+<button class="button" onclick="spam();">Send Message</button>
+    <h6 class="made">Made by <a href="https://github.com/TimsLab">TimsLab</a></h6>
+</div>
+<script>
+document.getElementById("sending").style.visibility = "hidden";
+document.getElementById("response").style.visibility = "hidden";
+
+
+function load() {
+  document.getElementById("sending").style.visibility = "visible";
+  
+  //request
+  var xhttp = new XMLHttpRequest();
+  xhttp.onreadystatechange = function() {
+	if (this.readyState == 4 && this.status == 200) {
+	  document.getElementById("sending").style.visibility = "hidden";
+	  document.getElementById("response").innerHTML = this.responseText;
+	  document.getElementById("response").style.visibility = "visible";
+	}
+  };
+  xhttp.open("POST", "api.php", true);
+  xhttp.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+  xhttp.send("load=1337");
+}
+
+function join() {
+  document.getElementById("sending").style.visibility = "visible";
+  
+  var invite = document.getElementById('invite').value;
+  
+  //request
+  var xhttp = new XMLHttpRequest();
+  xhttp.onreadystatechange = function() {
+	if (this.readyState == 4 && this.status == 200) {
+	  document.getElementById("sending").style.visibility = "hidden";
+	  document.getElementById("response").innerHTML = this.responseText;
+	  document.getElementById("response").style.visibility = "visible";
+	}
+  };
+  xhttp.open("POST", "api.php", true);
+  xhttp.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+  xhttp.send("join=" + invite);
+}
+
+function spam() {
+  document.getElementById("sending").style.visibility = "visible";
+  
+  var id = document.getElementById('id').value;
+  var message = document.getElementById('message').value;
+  
+  //request
+  var xhttp = new XMLHttpRequest();
+  xhttp.onreadystatechange = function() {
+	if (this.readyState == 4 && this.status == 200) {
+	  document.getElementById("sending").style.visibility = "hidden";
+	  document.getElementById("response").innerHTML = this.responseText;
+	  document.getElementById("response").style.visibility = "visible";
+	}
+  };
+  xhttp.open("POST", "api.php", true);
+  xhttp.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+  xhttp.send("channelid=" + id + "&message=" + message);
+}
+
+</script>
+</html>
